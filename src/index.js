ba9e4fa28849cc4cb033f5c7ebded21d9df09c0d
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2022-11-02 14:26:01 +0300

    trying to do my homework

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
+import App, { EditUserPage, HomePage, UserPage, UsersListPage } from "./App";
 import "./index.css";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App>
+        <Router history={history}>
+          <Switch>
+            <Route path="/users/:userId/edit" component={EditUserPage} />
+            <Route path="/users/:userId" component={UserPage} />
+            <Route path="/users" component={UsersListPage} />
+            <Route path="/" exact component={HomePage} />
+          </Switch>
+        </Router>
+      </App>
+    </BrowserRouter>
   </React.StrictMode>
 );
